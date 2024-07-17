475fac8a1370dc76a031fbd06b920d20a85528de
Heitor Schleder <100155822+heitorschleder@users.noreply.github.com>
2024-07-17 10:45:07 -0300

    fix responsive problems

--- components/restart/index.vue
+++ components/restart/index.vue
@@ -5,8 +5,8 @@ defineProps({
 });
 </script>
 <template>
-  <div class="flex-col xl:flex justify-center bg-slate-300 h-screen">
-    <div id="LeftPart" class="flex p-4 items-center bg-slate-300 ml-44">
+  <div class="grid justify-center xl:flex xl:justify-center bg-slate-300 xl:h-[100vh]">
+    <div id="LeftPart" class="flex p-4 items-center justify-center bg-slate-300">
       <div class="flex-col text-center justify-center items-center p-2 ">
         <img v-if="props.userPhoto" class="max-h-60 max-w-60 mb-6 rounded-full" :src="props.userPhoto" alt="userPhoto" />
         <h3>{{ props.userName }}</h3>
@@ -48,7 +48,7 @@ defineProps({
         </div>
       </div>
     </div>
-    <div id="MiddlePart" class="flex-row content-center text-center items-center p-4 h-[18rem] w-[60rem]">
+    <div id="MiddlePart" class="flex-row content-center text-center items-center p-2 xl:w-2/4">
       <div class="border rounded-md mt-2 z-[-1]">
         <AboutBrand :props="{
             aboutTitle: props.aboutTitle,
@@ -58,16 +58,16 @@ defineProps({
       <div id="Projects" class="border rounded-md mt-2 ">
         <section>
           <div class="text-black hover:border border-sky-500 duration-200">
-            <v-carousel hide-delimiters style="height: 100%">
+            <v-carousel hide-delimiters style="height: 90%">
               <v-carousel-item v-for="projects in props.projects" :key="projects.id" class="CardItem">
-                <div class="grid grid-rows-2 grid-flow-col gap-1">
+                <div class="flex flex-col md:grid md:grid-rows-2 md:grid-flow-col md:gap-1">
                   <img :src="projects.image" alt="imageProject"
-                    class="hover:animate-spin row-span-3 p-2 ml-2 h-64 mt-6 rounded-full object-contain object-center" />
+                    class="hover:scale-105 hover:animate-pulse duration-200 row-span-3 p-2 ml-2 h-64 mt-2 md:mt-6 rounded-full object-contain object-center" />
                   <div class="col-span-2 mt-5 ml-2">
                     <h2>{{ projects.title }}</h2>
-                    <p class="max-h-10 h-5">{{ projects.about }}</p>
+                    <p class="max-h-10 h-8">{{ projects.about }}</p>
                   </div>
-                  <div class="row-span-2 col-span-2 mt-10 ml-2">
+                  <div class="flex flex-row justify-center mb-2 md:row-span-2 md:col-span-2 md:mt-10 md:ml-2">
                     <div class="flex space-x-2 p-0">
                       <a class="hover:-translate-y-[2px]" :href="projects.linkView"><img
                           class="p-1 w-8 h-8 bg-[#172236] rounded-full" src="./images/eye.png" alt="the project" /></a>
@@ -81,16 +81,15 @@ defineProps({
           </div>
         </section>
       </div>
-      <div class="border rounded-md mt-2 ">
-        <section id="Certifies">
-          <div class="text-center max-h-80 hover:border border-sky-500 duration-200">
+      <div class="border rounded-md mt-2">
+        <section id="Certifies" class="hover:border border-sky-500 duration-200">
             <h3
-              class="mt-2 font-bold tracking-widest uppercase bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-violet-500">
+              class="mt-2 font-bold tracking-widest uppercase bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-violet-500 ">
               {{ props.titleCert }}
             </h3>
-            <div class="flex justify-center space-x-4">
+            <div class="flex flex-col p-2 items-center content-center space-y-4 justify-center md:flex-row md:space-x-4">
               <div class="flex-col text-center justify-center items-center">
-                <a class="text-slate-100 no-underline" href="https://google.com">
+                <a class="flex text-center justify-center items-center text-slate-100 no-underline" href="https://google.com">
                   <img class="max-w-[450px] max-h-[150px] rounded hover:scale-[1.1] duration-300"
                     src="https://matriculas.estacio.br/blog/wp-content/uploads/2020/02/30est01-ciencia-dados.jpg"
                     alt="" />
@@ -101,7 +100,7 @@ defineProps({
                 </div>
               </div>
               <div class="flex-col text-center justify-center items-center">
-                <a class="text-slate-100 no-underline" href="https://google.com">
+                <a class="flex text-center justify-center items-center text-slate-100 no-underline" href="https://google.com">
                   <img class="max-w-[350px] max-h-[150px] rounded hover:scale-[1.1] duration-300"
                     src="https://img-c.udemycdn.com/course/480x270/2037548_028f.jpg" alt="" />
                 </a>
@@ -111,7 +110,6 @@ defineProps({
                 </div>
               </div>
             </div>
-          </div>
         </section>
       </div>
     </div>
